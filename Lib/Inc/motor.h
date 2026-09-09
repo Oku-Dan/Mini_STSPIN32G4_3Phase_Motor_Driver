7f2ed7b58103c316feead56bb1e28d493a454d92
Oku-Dan <oku_dan411@yahoo.co.jp>
2026-09-10 00:30:28 +0900

    前のステージング漏れ

--- Lib/Inc/motor.h
+++ Lib/Inc/motor.h
@@ -7,7 +7,7 @@ void MotorInit(Motor_HandleTypeDef *handle);
 Voltage_TypeDef MotorUpdateController(Motor_HandleTypeDef *handle, float mech_angle, float mech_speed, float current_u, float current_v, float current_w);
 
 void MotorSetForcedSpeed(Motor_HandleTypeDef *handle, float speed, float voltage);
-void MotorSetForcedAngle(Motor_HandleTypeDef *handle, float angle, float voltage);
+void MotorSetForcedElecAngle(Motor_HandleTypeDef *handle, float elec_angle, float voltage);
 void MotorSetVoltage(Motor_HandleTypeDef *handle, float voltage_d, float voltage_q);
 void MotorSetTargetSpeed(Motor_HandleTypeDef *handle, float speed);
 void MotorSetTargetAngle(Motor_HandleTypeDef *handle, float angle);
